da63941cdec10fcaa5b767a016336e1abd2e093c
Diego Diaz <berith.py@gmail.com>
2017-04-22 19:34:15 -0400

    menu background tweening

--- src/sources/ludumdare38/States/menuState.js
+++ src/sources/ludumdare38/States/menuState.js
@@ -6,14 +6,16 @@ menuState.prototype =
             // Load assets
             game.load.image('title_background', 'src/graphics/title_screen.png');
             game.load.image('whole_screen', 'src/graphics/whole_screen.png');
-            game.load.image('logo', 'src/graphics/logo_placeholder.png');
+            game.load.image('logo', 'src/graphics/red_car.png');
         },
 
         create: function () {
             // game.add.plugin(PhaserInput.Plugin);
-            titleBackground = game.add.image(0, 0, 'title_background');
-            logoTop = game.add.image(30, 300, 'logo');
+            titleBackground = game.add.sprite(0, 0, 'title_background');
+            backgroundTween = game.add.tween(titleBackground);
+            backgroundTween.to({x:-600,y:-380}, 10000,'Linear',true,0);
 
+            logoTop = game.add.image(30, 300, 'logo');
             wholeScreen = game.add.sprite(0, 0, 'whole_screen');
             wholeScreen.inputEnabled = true;
             wholeScreen.events.onInputDown.add(this.start_game, this);
